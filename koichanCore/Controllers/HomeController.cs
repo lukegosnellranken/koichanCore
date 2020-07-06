dca7bd23dc9f0b4030e44d0e8760be57acad969c
Luke Gosnell <luke_gosnell@ranken.edu>
2020-07-05 21:39:52 -0500

    initial commit

---
+++ koichanCore/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using koichanCore.Models;
+
+namespace koichanCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult WebDev()
+        {
+            return View();
+        }
+
+        public ActionResult Software()
+        {
+            return View();
+        }
+
+        public ActionResult Hardware()
+        {
+            return View();
+        }
+
+        public ActionResult TechCulture()
+        {
+            return View();
+        }
+    }
+}
