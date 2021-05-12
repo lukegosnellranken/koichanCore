eadbea672a035b5bababf3a41f5b624929cb1f4b
Luke Gosnell <luke_gosnell@ranken.edu>
2021-05-11 23:40:02 -0500

    working links, js for aside display, logo

---
+++ koichanCore/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using koichanCore.Models;
+
+namespace koichanCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View("~/Areas/Identity/Pages/Account/Login.cshtml");
+        }
+
+        public ActionResult Register()
+        {
+            return View("~/Areas/Identity/Pages/Account/Register.cshtml");
+        }
+    }
+}
---
+++ koichanCore/wwwroot/js/layout.js
@@ -0,0 +1,9 @@
+﻿asideFunc() {
+    var ha = document.getElementById('homeAside');
+    if (ha.style.display === "none") {
+        ha.style.display = "block";
+    } else {
+        ha.style.display = "none";
+    }
+    console.log("example!")
+}
